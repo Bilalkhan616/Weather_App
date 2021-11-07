65bf2a8f660a7540e011e0e5b2c8d6b235f15bee
Bilalkhan616 <bilalkhan6v6@gmail.com>
2021-11-08 00:48:28 +0500

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState({});
+  const [search, setSearch] = useState('');
+
+  const weatherApi = async (data) => {
+    console.log(data)
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=71584c0ba104dde82413be284692fee7&units=metric`;
+    try {
+      let response = await axios.get(api, data);
+      console.log(response);
+      setCity(response.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="btn-container">
+        <button
+          type="submit"
+          className="search-btn"
+          onClick={weatherApi}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+        </div>
+      </div>
+      {(typeof city.main != "undefined") ? (
+        <div>
+          <div className="main-container">
+            <div className="city-box">
+              <div className="city">{city.name},{city.sys.country}</div>
+            </div>
+            <div className="weather-container">
+              <div className="temp-container">{city.main.temp}°c</div>
+              {/* <img src= {`http://openweathermap.org/img/wn/${city.weather[0].icon}/@2x.png`} alt={"city.weather[0].icon"}/> */}
+              <div className="condition-box">{city.weather[0].main}</div>
+            </div>
+          </div>
+        </div>
+      )
+        :
+        ("")
+      }
+
     </div>
   );
 }
